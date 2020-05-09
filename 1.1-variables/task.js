b11266f64e7b5a56c0ff8b3b6cf1519a0b91196e
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-05-09 10:00:45 +0800

    result commit

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -20,8 +20,12 @@ function sayHello(userName) {
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    // реализуйте здесь задание №3
+    let x = 2;
+    let y = 22;
+    let z = 0;
+
+    let result = x * y + 5 * z + x - 1;
+
+    return(result); // ваш ответ;
 }
