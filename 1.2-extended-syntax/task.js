97830a04b1ac031c60aa197febb6e4bfa6dd65ef
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-05-15 17:55:48 +0800

    the equation commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,34 @@
-function getResult(a,b,c){
+'use strict';
+
+function getResult(a, b, c){
     // код для задачи №1 писать здесь
-    // return x;
+    let x = [];
+    let d = b ** 2 - (4 * a * c);
+    if (d < 0) {
+        return x;
+    } else if (d === 0) {
+        let x1 = -b / (2 * a);
+        x[0] = x1;
+    } else {
+        let x1 = (-b + Math.sqrt(d)) / (2 * a);
+        let x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+        x[0] = x1;
+        x[1] = x2;
+        
+    }
+
+    return x;
 }
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
+function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
     // return result;
 }
+
+  
