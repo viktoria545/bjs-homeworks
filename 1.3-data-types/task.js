b90889d4f59ecb2f48f7e180bf40424d3868da6d
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-05-21 08:27:12 +0800

    calculator for calculating mortgage payments

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,32 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        return `Параметр ${percent} содержит неправильное значение ${percent}`
+    }
+    if (isNaN(contribution)) {
+        return `Параметр ${contribution} содержит неправильное значение ${contribution}`
+    }
+    if (isNaN(amount)) {
+        return `Параметр ${amount} содержит неправильное значение ${amount}`
+    }
+    if (isNaN(date)) {
+        return `Параметр ${date} содержит неправильное значение ${date}`
+    }
+
+    let S = amount - contribution;
+    date = new Date(date);
+    percent = percent / 100;
+    let P = percent / 12;
+    let n = ((date.getFullYear() - new Date().getFullYear()) * 12);
+    let totalAmount = parseFloat(((S * (P + P / (((1 + P) ** n) - 1))) *n ).toFixed(2));
+
+    console.log (totalAmount);
+     return totalAmount;
 }
 
+
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  
 }
