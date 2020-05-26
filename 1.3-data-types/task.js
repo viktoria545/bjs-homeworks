ab852e06883be6b8eba6402f81972368124aa5b6
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-05-26 15:42:24 +0800

    correction for const

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -14,12 +14,12 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
         return `Параметр ${date} содержит неправильное значение ${date}`
     }
 
-    let S = amount - contribution;
+   const S = amount - contribution;
     date = new Date(date);
     percent = percent / 100;
-    let P = percent / 12;
-    let n = ((date.getFullYear() - new Date().getFullYear()) * 12);
-    let totalAmount = parseFloat(((S * (P + P / (((1 + P) ** n) - 1))) *n ).toFixed(2));
+    const P = percent / 12;
+    const n = ((date.getFullYear() - new Date().getFullYear()) * 12);
+    const totalAmount = parseFloat(((S * (P + P / (((1 + P) ** n) - 1))) *n ).toFixed(2));
 
     console.log (totalAmount);
      return totalAmount;
