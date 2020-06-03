06b63b10c73a9a6ea8ba64e7e4e5a05b5dca278b
Viktoria Tretyak <vika.tretyak95.95@mail.ru>
2020-06-03 09:46:38 +0800

    tiger magazine coming of age commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,42 @@
 function getAnimalSound(animal) {
+    let sound = animal;
     // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    } else {
+        return animal.sound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+
+	 if (marks.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+
+    }
+    let average = Math.round(sum / marks.length);
+
+    let roundedAverage = average;
+    return roundedAverage;
+ }
+
+
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Number(new Date());
+    birthday = Number(new Date(birthday));
+    let diff = now - birthday;
+    let milliseconds = 365.25 * 24 * 60 * 60 * 1000;
+    let age  = diff / milliseconds ;
+    return age >= 18;
 }
+    
+
+
+
